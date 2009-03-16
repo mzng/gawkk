54f6a147ada6b38b2eb40cef33c398188eea053c
Thomas Mango <tsmango@gmail.com>
2009-03-16 17:49:40 -0400

    Remember login now intentionally ignored to stop no login error.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -10,16 +10,16 @@ class AuthenticationController < ApplicationController
     else
       if @user = User.new(params[:user]).try_to_login
         # Setup cookies for automatic logins
-        if params[:remember]
-          begin
-            cookie_pass = [Array.new(9){rand(256).chr}.join].pack("m").chomp
-            cookie_hash = Digest::MD5.hexdigest(cookie_pass + @user.salt)
-            cookies[:gawkk_login_pass] = { :value => cookie_pass, :expires => 2.weeks.from_now }
-            cookies[:gawkk_login] = { :value => @user.username, :expires => 2.weeks.from_now }
-            @user.cookie_hash = cookie_hash
-          rescue
-          end
-        end
+        # if params[:remember]
+        #   begin
+        #     cookie_pass = [Array.new(9){rand(256).chr}.join].pack("m").chomp
+        #     cookie_hash = Digest::MD5.hexdigest(cookie_pass + @user.salt)
+        #     cookies[:gawkk_login_pass] = { :value => cookie_pass, :expires => 2.weeks.from_now }
+        #     cookies[:gawkk_login] = { :value => @user.username, :expires => 2.weeks.from_now }
+        #     @user.cookie_hash = cookie_hash
+        #   rescue
+        #   end
+        # end
         
         # Update the user's last login time
         @user.last_login_at = Time.new
