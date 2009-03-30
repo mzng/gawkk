42f2d08d1d60f14d2782dffefd0408f3e79cb1d4
Thomas Mango <tsmango@gmail.com>
2009-03-29 20:01:39 -0400

    Fixed BackupMailer to deliver to logs@gawkk.com rather than tom@gawkk.com.

--- app/models/backup_mailer.rb
+++ app/models/backup_mailer.rb
@@ -2,7 +2,7 @@ class BackupMailer < ActionMailer::Base
   def status_update(files)
     @subject    = "Amazon S3: Backup Status (#{files[:database].size} DB Backups; #{files[:images].size} New Images)"
     @body["files"] = files
-    @recipients = "tom@gawkk.com"
+    @recipients = "logs@gawkk.com"
     @from       = "\"Gawkk\" <notifier@gawkk.com>"
     headers       "Reply-to" => "notifier@gawkk.com"
     @sent_on    = Time.now
