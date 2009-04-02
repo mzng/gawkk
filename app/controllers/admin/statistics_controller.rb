5e9392970ada3be0bf66143a4c8ec2fb12886b0d
Thomas Mango <tsmango@gmail.com>
2009-04-02 17:16:33 -0400

    * Added a Statistics > Likes page with per day and per week graphs.

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -7,24 +7,6 @@ class Admin::StatisticsController < ApplicationController
     
   end
   
-  def registrations
-    @total = User.count(:all, :conditions => {:feed_owner => false})
-    @total_today = User.count(:all, :conditions => ['feed_owner = false AND date(created_at) = curdate()'])
-    
-    registrations_per_day = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, dayofyear(created_at) as dayofyear", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, dayofyear', :order => 'created_at')
-    registration_counts_per_day = registrations_per_day.collect{|summary| summary.registrations.to_i}
-    registration_months_per_day = blank_duplicate_recurring_months(registrations_per_day.collect{|summary| summary.month + '/' + summary.year})
-    
-    @per_day = GChart.line(:data => [registration_counts_per_day], :extras => {'chtt' => 'Per Day', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_day.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_day.max}"})
-    
-    
-    registrations_per_week = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, week(created_at) as week", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, week', :order => 'created_at')
-    registration_counts_per_week = registrations_per_week.collect{|summary| summary.registrations.to_i}
-    registration_months_per_week = blank_duplicate_recurring_months(registrations_per_week.collect{|summary| summary.month + '/' + summary.year})
-    
-    @per_week = GChart.line(:data => [registration_counts_per_week], :extras => {'chtt' => 'Per Week', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_week.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_week.max}"})
-  end
-  
   def comments
     @total = Comment.count(:all)
     @total_today = Comment.count(:all, :conditions => ['date(created_at) = curdate()'])
@@ -41,6 +23,42 @@ class Admin::StatisticsController < ApplicationController
     comment_months_per_week = blank_duplicate_recurring_months(comments_per_week.collect{|summary| summary.month + '/' + summary.year})
     
     @per_week = GChart.line(:data => [comment_counts_per_week], :extras => {'chtt' => 'Per Week', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{comment_months_per_week.join('|')}|", 'chxr' => "1,0,#{comment_counts_per_week.max}"})
+  end
+  
+  def likes
+    @total = Like.count(:all)
+    @total_today = Like.count(:all, :conditions => ['date(created_at) = curdate()'])
+    
+    likes_per_day = Like.find(:all, :select => "count(*) as likes, year(created_at) as year, month(created_at) as month, day(created_at) as day, dayofyear(created_at) as dayofyear", :conditions => ['created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, dayofyear', :order => 'created_at')
+    like_counts_per_day = likes_per_day.collect{|summary| summary.likes.to_i}
+    like_months_per_day = blank_duplicate_recurring_months(likes_per_day.collect{|summary| summary.month + '/' + summary.year})
+    
+    @per_day = GChart.line(:data => [like_counts_per_day], :extras => {'chtt' => 'Per Day', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{like_months_per_day.join('|')}|", 'chxr' => "1,0,#{like_counts_per_day.max}"})
+    
+    
+    likes_per_week = Like.find(:all, :select => "count(*) as likes, year(created_at) as year, month(created_at) as month, day(created_at) as day, week(created_at) as week", :conditions => ['created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, week', :order => 'created_at')
+    like_counts_per_week = likes_per_week.collect{|summary| summary.likes.to_i}
+    like_months_per_week = blank_duplicate_recurring_months(likes_per_week.collect{|summary| summary.month + '/' + summary.year})
+    
+    @per_week = GChart.line(:data => [like_counts_per_week], :extras => {'chtt' => 'Per Week', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{like_months_per_week.join('|')}|", 'chxr' => "1,0,#{like_counts_per_week.max}"})
+  end
+  
+  def registrations
+    @total = User.count(:all, :conditions => {:feed_owner => false})
+    @total_today = User.count(:all, :conditions => ['feed_owner = false AND date(created_at) = curdate()'])
+    
+    registrations_per_day = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, dayofyear(created_at) as dayofyear", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, dayofyear', :order => 'created_at')
+    registration_counts_per_day = registrations_per_day.collect{|summary| summary.registrations.to_i}
+    registration_months_per_day = blank_duplicate_recurring_months(registrations_per_day.collect{|summary| summary.month + '/' + summary.year})
+    
+    @per_day = GChart.line(:data => [registration_counts_per_day], :extras => {'chtt' => 'Per Day', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_day.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_day.max}"})
+    
+    
+    registrations_per_week = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, week(created_at) as week", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, week', :order => 'created_at')
+    registration_counts_per_week = registrations_per_week.collect{|summary| summary.registrations.to_i}
+    registration_months_per_week = blank_duplicate_recurring_months(registrations_per_week.collect{|summary| summary.month + '/' + summary.year})
+    
+    @per_week = GChart.line(:data => [registration_counts_per_week], :extras => {'chtt' => 'Per Week', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_week.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_week.max}"})
   end
   
   
