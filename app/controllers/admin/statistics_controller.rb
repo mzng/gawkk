b611cbd51c7b5ec6917c591eac5d5a933f00ee02
Thomas Mango <tsmango@gmail.com>
2009-04-02 10:58:41 -0400

    Updated how labels are displayed on the registration graphs.

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -9,14 +9,15 @@ class Admin::StatisticsController < ApplicationController
     
     registrations_per_day = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, dayofyear(created_at) as dayofyear", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, dayofyear', :order => 'created_at')
     registration_counts_per_day = registrations_per_day.collect{|summary| summary.registrations.to_i}
-    registration_months_per_day = registrations_per_day.collect{|summary| summary.month + '/' + summary.year}.uniq
+    registration_months_per_day = blank_duplicate_recurring_months(registrations_per_day.collect{|summary| summary.month + '/' + summary.year})
+    
     
     @per_day = GChart.line(:data => [registration_counts_per_day], :extras => {'chtt' => 'Per Day', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_day.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_day.max}"})
     
     
     registrations_per_week = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, week(created_at) as week", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, week', :order => 'created_at')
     registration_counts_per_week = registrations_per_week.collect{|summary| summary.registrations.to_i}
-    registration_months_per_week = registrations_per_week.collect{|summary| summary.month + '/' + summary.year}.uniq
+    registration_months_per_week = blank_duplicate_recurring_months(registrations_per_week.collect{|summary| summary.month + '/' + summary.year})
     
     @per_week = GChart.line(:data => [registration_counts_per_week], :extras => {'chtt' => 'Per Week', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_week.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_week.max}"})
   end
@@ -29,5 +30,18 @@ class Admin::StatisticsController < ApplicationController
     else
       redirect_to '/'
     end
+  end
+  
+  def blank_duplicate_recurring_months(months)
+    last = ''
+    months.each_with_index do |month, i|
+      if month == last
+        months[i] = ''
+      else
+        last = month
+      end
+    end
+    
+    return months
   end
 end
