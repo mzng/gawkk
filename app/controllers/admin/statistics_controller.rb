44db61492c1dfaec1bf001c940d3c2bd1a178c32
Thomas Mango <tsmango@gmail.com>
2009-04-02 00:04:53 -0400

    * Added a second graph on the registrations page that shows registrations per week

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -6,11 +6,19 @@ class Admin::StatisticsController < ApplicationController
   def registrations
     @total = User.count(:all, :conditions => {:feed_owner => false})
     
-    registrations_per_day = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, dayofyear(created_at) as dayofyear", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, dayofyear', :order => 'created_at')
-    registration_counts = registrations_per_day.collect{|summary| summary.registrations.to_i}
-    registration_months = registrations_per_day.collect{|summary| summary.month + '/' + summary.year}.uniq
     
-    @chart = GChart.line(:data => [registration_counts], :extras => {'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months.join('|')}|", 'chxr' => "1,0,#{registration_counts.max}"})
+    registrations_per_day = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, dayofyear(created_at) as dayofyear", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, dayofyear', :order => 'created_at')
+    registration_counts_per_day = registrations_per_day.collect{|summary| summary.registrations.to_i}
+    registration_months_per_day = registrations_per_day.collect{|summary| summary.month + '/' + summary.year}.uniq
+    
+    @per_day = GChart.line(:data => [registration_counts_per_day], :extras => {'chtt' => 'Per Day', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_day.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_day.max}"})
+    
+    
+    registrations_per_week = User.find(:all, :select => "count(*) as registrations, year(created_at) as year, month(created_at) as month, day(created_at) as day, week(created_at) as week", :conditions => ['feed_owner = false and created_at > date_sub(curdate(), interval 6 month)'], :group => 'year, week', :order => 'created_at')
+    registration_counts_per_week = registrations_per_week.collect{|summary| summary.registrations.to_i}
+    registration_months_per_week = registrations_per_week.collect{|summary| summary.month + '/' + summary.year}.uniq
+    
+    @per_week = GChart.line(:data => [registration_counts_per_week], :extras => {'chtt' => 'Per Week', 'chs' => '900x300', 'chxt' => 'x,y', 'chxl' => "0:|#{registration_months_per_week.join('|')}|", 'chxr' => "1,0,#{registration_counts_per_week.max}"})
   end
   
   
