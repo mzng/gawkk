7da1667f0d9d8072412d6d39c9b07ed63b8c32c3
Thomas Mango <tsmango@gmail.com>
2009-05-21 00:27:08 -0400

    Updated production facebook api keys.

--- app/models/util/facebook.rb
+++ app/models/util/facebook.rb
@@ -3,8 +3,8 @@ class Util::Facebook
     config = Hash.new
     
     if Rails.env.production?
-      config[:key]    = ''
-      config[:secret] = ''
+      config[:key]    = '3b73b34a3d750d20a4f4e86905459ad1'
+      config[:secret] = '3d4b4a03e7a18aaf52201885c389535b'
     else
       config[:key]    = '6f43f1ad90bf57624b102ad2e7a59331'
       config[:secret] = '49b6081049b801c74fffae0fe4292092'
