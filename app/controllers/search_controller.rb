5cd699e01218ebc1519846ba19aa68e99df2ac32
Thomas Mango <tsmango@gmail.com>
2009-07-23 13:19:39 -0400

    Reversed :sort_mode changes because I'm an idiot.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,7 +11,7 @@ class SearchController < ApplicationController
     
     @channels = Channel.search(@q.split.join(' | '), :page => @page, :per_page => 7, :conditions => {:user_owned => false}, :match_mode => :boolean, :retry_stale => true)
     @users = User.search(@q, :page => @page, :per_page => 7, :conditions => {:feed_owner => false}, :retry_stale => true)
-    @videos = Video.search(@q, :order => :posted_at, :sort_mode => 'posted_at DESC', :page => @page, :per_page => 5, :conditions => {:category_id => Category.allowed_on_front_page_ids}, :retry_stale => true)
+    @videos = Video.search(@q, :order => :posted_at, :sort_mode => :desc, :page => @page, :per_page => 5, :conditions => {:category_id => Category.allowed_on_front_page_ids}, :retry_stale => true)
   end
   
   def channels
@@ -35,7 +35,7 @@ class SearchController < ApplicationController
     setup_generic_sidebar
     setup_user_sidebar(logged_in_user) if user_logged_in?
     
-    @videos = Video.search(@q, :order => :posted_at, :sort_mode => 'posted_at DESC', :page => @page, :per_page => @per_page, :conditions => {:category_id => Category.allowed_on_front_page_ids}, :retry_stale => true)
+    @videos = Video.search(@q, :order => :posted_at, :sort_mode => :desc, :page => @page, :per_page => @per_page, :conditions => {:category_id => Category.allowed_on_front_page_ids}, :retry_stale => true)
   end
   
   def youtube
