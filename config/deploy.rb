49cb5aa0f402e825f072d197b06172a414f6d196
s.hoffmann <s.hoffmann@codingefficiency.com>
2010-12-03 18:38:59 +0100

    staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,187 +1,29 @@
-require 'railsmachine/recipes'
-
-# This defines a deployment "recipe" that you can feed to capistrano
-# (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
-# (among other things) the deployment of your application.
-
-# =============================================================================
-# REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
-
-
-
-# Deployment Paramenters
-set :github_username, 'sergeyrgv'
-set :github_repository_name, 'gawkk'
-
-
-# The name of your application. Used for directory and file names associated with
-# the application.
 set :application, "gawkk"
+set :repository,  "git@github.com:mzng/gawkk.git"
+set :branch, "staging"
 
-# Target directory for the application on the web and app servers.
+set :scm, :git
+
 set :deploy_to, "/var/www/apps/#{application}"
+set :user, :root
 
-# Primary domain name of your application. Used as a default for all server roles.
-set :domain, "gawkk.com"
+set :environment, :staging
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-# Login user for ssh.
-set :user, "deploy"
-set :runner, "deploy"
-set :admin_runner, "deploy"
+role :web, "85.214.99.61"                          # Your HTTP server, Apache/etc
+role :app, "85.214.99.61"                          # This may be the same as your `Web` server
+role :db,  "85.214.99.61", :primary => true # This is where Rails migrations will run
 
-
-# URL of your source repository.
-set :repository, "git@github.com:mzng/#{github_repository_name}.git"
-
-# Rails environment. Used by application setup tasks and migrate tasks.
-set :rails_env, "production"
-
-# Automatically symlink these directories from curent/public to shared/public.
-set :app_symlinks, %w{images/categories images/thumbnails images/users sitemaps}
-
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
-
-# Modify these values to execute tasks on a different server.
-role :web, "204.188.244.130"
-role :app, "204.188.244.130"
-role :db,  "204.188.244.130", :primary => true
-
-# =============================================================================
-# APACHE OPTIONS
-# =============================================================================
-# set :apache_server_name, domain
-# set :apache_server_aliases, %w{alias1 alias2}
-set :apache_default_vhost, true # force use of apache_default_vhost_config
-# set :apache_default_vhost_conf, "/etc/httpd/conf/default.conf"
-# set :apache_conf, "/etc/httpd/conf/apps/#{application}.conf"
-# set :apache_ctl, "/etc/init.d/httpd"
-# set :apache_proxy_port, 8000
-# set :apache_proxy_servers, 20
-# set :apache_proxy_address, "127.0.0.1"
-# set :apache_ssl_enabled, false
-# set :apache_ssl_ip, "127.0.0.1"
-# set :apache_ssl_forward_all, false
-
-# =============================================================================
-# PASSENGER OPTIONS
-# =============================================================================
-set :app_server, :passenger
-
-# =============================================================================
-# MONGREL OPTIONS
-# =============================================================================
-# set :mongrel_servers, apache_proxy_servers
-# set :mongrel_port, apache_proxy_port
-# set :mongrel_address, apache_proxy_address
-# set :mongrel_environment, "production"
-# set :mongrel_pid_file, "/var/run/mongrel_cluster/#{application}.pid"
-# set :mongrel_conf, "/etc/mongrel_cluster/#{application}.conf"
-# set :mongrel_user, user
-# set :mongrel_group, group
-
-# =============================================================================
-# SCM OPTIONS
-# =============================================================================
-set :scm, "git"
-
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
-
-# =============================================================================
-# CAPISTRANO OPTIONS
-# =============================================================================
-# default_run_options[:pty] = true
-set :keep_releases, 5
-set :scm_username, "#{github_username}"
-set :scm_password, "deploy1234"
-set :branch, "master"
-set :deploy_via, :remote_cache
-set :git_enable_submodules, 1
-
-# =============================================================================
-# ADDITIONAL TASKS
-# =============================================================================
-# namespace :deploy do
-#   desc "Runs asset:packager"
-#   task :after_update_code, :roles => :web do
-#     run <<-EOF
-#       cd #{release_path} && rake RAILS_ENV=production asset:packager:build_all
-#     EOF
-#   end
-# end
-
-task :move_sitemaps, :roles => :web do
-  #run "cp /var/www/apps/gawkk/shared/public/sitemaps/*.xml.gz /var/www/apps/gawkk/current/public/"
-end
-
-task :move_dispatchs, :roles => :web do
-  run "cp /var/www/apps/gawkk/dispatch/* /var/www/apps/gawkk/current/public/"
-end
-
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
-  desc "Runs asset:packager"
-  task :after_update_code, :roles => :web do
-    run <<-EOF
-      cd #{release_path} && rake RAILS_ENV=production asset:packager:build_all
-    EOF
-  end
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
 end
 
-namespace :app do
-  namespace :symlinks do
-    desc "Ensures directories for symlinking are available"
-    task :setup_db, :roles => :db do
-      if app_symlinks
-        app_symlinks.each {|link| run "mkdir -p #{shared_path}/public/#{link}"}
-      end
-    end
 
-    desc "Links public directories to shared location"
-    task :update_db, :roles => :db do
-      if app_symlinks
-        app_symlinks.each {|link| run "ln -nfs #{shared_path}/public/#{link} #{current_path}/public/#{link}"}
-      end
-    end
-  end
-  
-  namespace :maintenance do
-    task :enable, :roles => :web do
-      run "mv #{shared_path}/system/.maintenance.html #{shared_path}/system/maintenance.html"
-    end
-    
-    task :disable, :roles => :web do
-      run "mv #{shared_path}/system/maintenance.html #{shared_path}/system/.maintenance.html"
-    end
-  end
-end
-
-namespace :memcached do
-  task :start, :roles => [:web, :db] do
-    sudo "/etc/init.d/memcached start"
-  end
-  
-  task :stop, :roles => [:web, :db] do
-    sudo "/etc/init.d/memcached stop"
-  end
-end
-
-after 'app:symlinks:setup',  'app:symlinks:setup_db'
-after 'app:symlinks:update', 'app:symlinks:update_db'
-after 'deploy', 'move_sitemaps'
-after 'deploy', 'move_dispatchs'
---
+++ config/environments/staging.rb
@@ -0,0 +1,73 @@
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+
+# See everything in the log (default is :info)
+# config.log_level = :debug
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Use a different cache store in production
+#config.cache_store = :mem_cache_store, '204.188.244.130', '204.188.244.130', {:namespace => 'gawkk'}
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+config.action_mailer.perform_deliveries = false
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = { 
+   :address => "mail.gawkk.com", 
+   :port => 587, 
+   :domain => "gawkk.com", 
+   :authentication => :login, 
+   :user_name => "notifier", 
+   :password => "purplemonkeydishwasher"   
+}
+
+# Enable threaded mode
+# config.threadsafe!
+
+config.cache_store = :mem_cache_store
+memcache_options = {
+  :c_threshold => 10000,
+  :compression => true,
+  :debug => false,
+  :namespace => 'a',
+  :readonly => false,
+  :urlencode => false
+
+}
+
+BASE_URL = "gawkk.codingefficiency.com"
+BASE_URL_SIZE = 1
+
+require 'memcache'
+
+# make a CACHE global to use in your controllers instead of Rails.cache, this will use the new memcache-client 1.7.2
+CACHE = MemCache.new memcache_options
+
+# connect to your server that you started earlier
+CACHE.servers = '127.0.0.1:11211'
+
+# this is where you deal with passenger's forking
+begin
+   PhusionPassenger.on_event(:starting_worker_process) do |forked|
+     if forked
+       # We're in smart spawning mode, so...
+       # Close duplicated memcached connections - they will open themselves
+       CACHE.reset
+     end
+   end
+# In case you're not running under Passenger (i.e. devmode with mongrel)
+rescue NameError => error
+
+end
