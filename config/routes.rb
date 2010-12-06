e3a50ae25a6368ff9f82c8a3a5cbbadbd299960b
s.hoffmann <s.hoffmann@codingefficiency.com>
2010-12-06 15:27:23 +0100

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,15 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/channels', :controller => 'channels', :action => 'index', :category => 'movies-previews-trailers', :conditions => { :subdomain => "movies" }
   map.connect '/:id/discuss', :controller => 'videos', :action => 'discuss', :conditions => { :subdomain => "movies" }
   map.connect '/:user', :controller => 'channels', :action => 'show', :category => 'movies-previews-trailers', :channel => 'channel', :conditions => { :subdomain => "movies" }
-
+# videos
+  map.connect 'v/:id',                        :controller => 'videos', :action => 'follow'
+  map.connect ':id/discuss',                  :controller => 'videos', :action => 'discuss' 
+  map.connect ':id/watch',                    :controller => 'videos', :action => 'watch'
+  map.connect ':id/like',                     :controller => 'videos', :action => 'like'
+  map.connect ':id/dislike',                  :controller => 'videos', :action => 'dislike'
+  map.connect ':id/unlike',                   :controller => 'videos', :action => 'unlike'
+  map.connect ':id/comment',                  :controller => 'videos', :action => 'comment'
+  map.connect ':id/share',                    :controller => 'videos', :action => 'share'
   #legacy routes
   map.connect 'all/newest',               :controller => 'redirections', :action => 'all_newest'
   map.connect 'all/popular',              :controller => 'redirections', :action => 'all_popular'
@@ -120,15 +128,7 @@ ActionController::Routing::Routes.draw do |map|
   
     
   
-  # videos
-  map.connect 'v/:id',                        :controller => 'videos', :action => 'follow'
-  map.connect ':id/discuss',                  :controller => 'videos', :action => 'discuss' 
-  map.connect ':id/watch',                    :controller => 'videos', :action => 'watch'
-  map.connect ':id/like',                     :controller => 'videos', :action => 'like'
-  map.connect ':id/dislike',                  :controller => 'videos', :action => 'dislike'
-  map.connect ':id/unlike',                   :controller => 'videos', :action => 'unlike'
-  map.connect ':id/comment',                  :controller => 'videos', :action => 'comment'
-  map.connect ':id/share',                    :controller => 'videos', :action => 'share'
+  
 
   map.connect 'topics/:category.rss',         :controller => 'videos', :action => 'category', :format => "rss"
   map.connect 'topics/:category/channels',    :controller => 'channels', :action => 'index'
